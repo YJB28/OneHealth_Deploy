7b7af86b047bcbea5859f676038501c839cc259c
YogeshBaiskar <yogeshjagdev@infobellit.com>
2023-08-06 20:25:45 +0530

    user

--- OneHealth-PatientUser/src/main/java/com/onehealth/patientuser/controller/PatientUserController.java
+++ OneHealth-PatientUser/src/main/java/com/onehealth/patientuser/controller/PatientUserController.java
@@ -14,7 +14,7 @@ import com.onehealth.patientuser.service.PatientUserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/oneHealth-patientuser")
+@RequestMapping("/onehealth-patientuser")
 public class PatientUserController {
 
 	@Autowired
