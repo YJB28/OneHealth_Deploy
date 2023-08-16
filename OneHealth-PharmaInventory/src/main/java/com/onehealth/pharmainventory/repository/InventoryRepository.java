c599e97b54210898f483eda35204c8d8fb2142b7
YogeshBaiskar <yogeshjagdev@infobellit.com>
2023-08-16 18:02:39 +0530

    Pharma Added 1

--- OneHealth-PharmaInventory/src/main/java/com/onehealth/pharmainventory/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onehealth.pharmainventory.repository;
-
-import com.onehealth.pharmainventory.entity.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
-}
